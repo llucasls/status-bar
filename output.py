3cf26c8c9600e59835e9f3c9d5eb8700383d3527
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-08-14 05:11:33 -0300

    feat: print status bar output

--- output.py
+++ output.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 import time
+import os
 
-import psutil
+from status import status
 
 
-system_info = {
-    "CPU": psutil.cpu_percent(interval=0.3),
-    "RAM": psutil.virtual_memory().percent,
-    "Disk": psutil.disk_usage("/").percent,
-    "Battery": psutil.sensors_battery().percent,
-}
-
-for label, value in system_info.items():
-    print(f"{label}: {value}%")
-
-print(time.strftime("%A %d/%m/%Y %R"))
+while os.environ["DESKTOP_SESSION"] == "dwm":
+    output = " | ".join(status)
+    print(output)
+    time.sleep(1)
