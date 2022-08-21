ebd49279963988c0878fda317944eec0014fef7c
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-08-21 05:15:23 -0300

    feat: create new script to start status bar

---
+++ src/start.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from os import environ
+from time import sleep
+from subprocess import run, check_output
+
+
+while environ.get("DESKTOP_SESSION") == "dwm":
+    output = check_output(["status-bar-output"]).decode("UTF-8").strip()
+    command = ["xsetroot", "-name", output]
+    run(command)
+    sleep(5)
