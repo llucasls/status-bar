0e10765ed33c4ea9c4747cc19681c50a4ccc1ab6
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-08-28 03:13:15 -0300

    fix: close process when leaving dwm session

--- src/start.py
+++ src/start.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 from os import environ
 from time import sleep
+from sys import exit
 from subprocess import run, check_output
 
 
 while environ.get("DESKTOP_SESSION") == "dwm":
+    dwm_is_running = run("ps -e | grep dwm", shell=True).returncode == 0
+    if not dwm_is_running:
+        exit(0)
     output = check_output(["status-bar-output"]).decode("UTF-8").strip()
     command = ["xsetroot", "-name", output]
     run(command)
