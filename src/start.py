004fa38422f2a2df140257015ee273e67d1518d2
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-25 14:37:46 -0300

    style: add newlines

--- src/start.py
+++ src/start.py
@@ -8,9 +8,12 @@ from status import status
 
 while environ.get("DESKTOP_SESSION") == "dwm":
     dwm_is_running = run("ps -e | grep dwm", shell=True).returncode == 0
+
     if not dwm_is_running:
         exit(0)
+
     output = status()
     command = ["xsetroot", "-name", output]
     run(command)
+
     sleep(1.25)
