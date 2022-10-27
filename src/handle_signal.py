7b19ee16781ddb78274942c82adcc412e7385f06
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-27 14:29:07 -0300

    feat: customize handler with environment variables
    New variables were added: STATUS_BAR_NOTIFY and
    STATUS_BAR_NOTIFICATION_TIME. They state wether the bar should give a
    notification when the process is terminated, and the duration of the
    notification, respectively.

--- src/handle_signal.py
+++ src/handle_signal.py
@@ -1,17 +1,26 @@
+import os
 from subprocess import run
 from signal import signal, SIGHUP, SIGINT, SIGTERM
 from collections import defaultdict
+import json
 
 
 signals = defaultdict(lambda: "exit",
                       [(1, "SIGHUP"), (2, "SIGINT"), (15, "SIGTERM")])
 
+STATUS_BAR_NOTIFY = os.environ.get("STATUS_BAR_NOTIFY", "true")
+STATUS_BAR_NOTIFICATION_TIME = os.environ.get("STATUS_BAR_NOTIFICATION_TIME",
+                                              "10")
 
-def notify(summary, body=None):
+should_notify = json.loads(STATUS_BAR_NOTIFY)
+notification_time = json.loads(STATUS_BAR_NOTIFICATION_TIME) * 1000
+
+
+def notify(summary, body=None, expire_time=10_000):
     if body is None:
-        command = ["notify-send", "--expire-time=10000", summary]
+        command = ["notify-send", "-t", expire_time, summary]
     else:
-        command = ["notify-send", "--expire-time=10000", summary, body]
+        command = ["notify-send", "-t", expire_time, summary, body]
 
     run(command)
 
@@ -19,7 +28,8 @@ def notify(summary, body=None):
 def handler(signum, frame):
     if should_notify:
         notify("Signal Handler",
-               f"Signal handler called with signal {signals[signum]}")
+               f"Signal handler called with signal {signals[signum]}",
+               expire_time=notification_time)
 
     run(["xsetroot", "-name", ""])
 
