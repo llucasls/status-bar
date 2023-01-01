c8b78b4a28db0a0bebce579c3c9ccb4909903953
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-01 17:26:08 -0300

    feat: create function to print ram on Linux

--- src/ram.py
+++ src/ram.py
@@ -2,7 +2,25 @@
 import platform
 
 
-def read_meminfo():
+def read_meminfo_linux():
+    result = {}
+    with open("/proc/meminfo", "r") as meminfo:
+        for line in meminfo.readlines():
+            label, value, *unit = line.strip().split()
+            label = label.removesuffix(":")
+            value = int(value)
+            try:
+                if unit[0] == "kB":
+                    value *= 1024
+            except IndexError:
+                pass
+
+            result[label] = value
+
+    return result
+
+
+def read_meminfo_netbsd():
     result = {}
     with open("/proc/meminfo", "r") as meminfo:
         counter = 0
@@ -19,12 +37,28 @@ def read_meminfo():
 
             result[label] = value
 
-
     return result
 
 
-def ram():
-    memory = read_meminfo()
+def ram_linux():
+    memory = read_meminfo_linux()
+
+    mem_total = memory["MemTotal"]
+    mem_free = memory["MemFree"]
+    buffers = memory["Buffers"]
+    cached = memory["Cached"]
+    mem_used = mem_total - mem_free - buffers - cached
+
+    mem_percent = mem_used / mem_total * 100
+
+    if mem_percent < 100:
+        return "{:>4.1f}".format(mem_percent)
+
+    return " 100"
+
+
+def ram_netbsd():
+    memory = read_meminfo_netbsd()
 
     mem_total = memory["MemTotal"]
     mem_free = memory["MemFree"]
@@ -38,6 +72,14 @@ def ram():
     return " 100"
 
 
+def ram():
+    if platform.uname().system == "Linux":
+        return ram_linux()
+    elif platform.uname().system == "NetBSD":
+        return ram_netbsd()
+
+    return None
+
+
 if __name__ == "__main__":
-    if platform.uname().system == "NetBSD":
-        print(ram())
+    print(ram())
