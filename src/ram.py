026eca99f9290e569c6271fec2d1eb9420b885e7
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-01 13:10:55 -0300

    feat: create function to read /proc/meminfo

--- src/ram.py
+++ src/ram.py
@@ -6,5 +6,14 @@ def ram():
     return(psutil.virtual_memory().percent)
 
 
+def proc():
+    result = []
+    with open("/proc/meminfo", "r") as meminfo:
+        for line in meminfo.readlines():
+            result.append(line)
+
+    return result
+
+
 if __name__ == "__main__":
     print(ram())
