5af26dfaf0f36c5db35a9a9164cd676f9c92e38e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-02 12:11:38 -0300

    feat: return battery level on NetBSD

--- src/battery.py
+++ src/battery.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
+import platform
+from subprocess import check_output
 import psutil
 
 
-def battery():
-    """Returns battery level and charging status.
-    If no battery is found, returns None."""
+def battery_netbsd():
+    ENVSTAT = "/usr/sbin/envstat"
+    command = [ENVSTAT, "-s", "acpibat0:charge"]
 
+    command_output = check_output(command).decode("UTF-8")
+    output_lines = command_output.strip().split("\n")
+
+    result = output_lines[2].split("(")[1].split("%)")[0]
+
+    return result
+
+
+def battery_linux():
     try:
         percent, *bat, power_plugged = psutil.sensors_battery()
         percent = round(percent, 2)
@@ -18,6 +29,18 @@ def battery():
         output = None
 
     return(output)
+
+
+def battery():
+    """Returns battery level and charging status.
+    If no battery is found, returns None."""
+
+    if platform.uname().system == "Linux":
+        return battery_linux()
+    elif platform.uname().system == "NetBSD":
+        return battery_netbsd()
+
+    return None
 
 
 if __name__ == "__main__":
